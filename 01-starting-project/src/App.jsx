1c673a56f8beef0a041be642cad39feecb8a5f50
Eduardaskuliesa <eduardas2000@mail.ru>
2024-05-28 22:28:58 +0300

    feat: Finished First chapter React esentials

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,18 @@
-import CoreConcept from "./CoreConcept";
-import Header from "./Header";
+import { useState } from "react";
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data-with-examples";
 
 function App() {
+  const [topic, setTopic] = useState("");
+
+  const handleSelect = (selectedBtn) => {
+    setTopic(selectedBtn);
+    console.log(selectedBtn);
+  };
+
   return (
     <div>
       <Header />
@@ -13,12 +23,50 @@ function App() {
             {CORE_CONCEPTS.map((cocept, index) => (
               <CoreConcept
                 key={index}
-                title={cocept.title}
-                image={cocept.image}
-                description={cocept.description}
+                {...cocept}
               />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={topic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={topic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={topic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={topic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {!topic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[topic].title}</h3>
+              <p>{EXAMPLES[topic].description}</p>
+              <pre>
+                <code>{EXAMPLES[topic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
